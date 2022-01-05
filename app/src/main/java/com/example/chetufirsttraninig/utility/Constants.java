96f61d484b0cda65a4831a68ddee61f1f896e216
Administrator <guddupasi7@gmail.com>
2022-01-05 22:04:04 +0530

    intent continue...

---
+++ app/src/main/java/com/example/chetufirsttraninig/utility/Constants.java
@@ -0,0 +1,10 @@
+package com.example.chetufirsttraninig.utility;
+
+public class Constants {
+    public static final String FNAME = "fname";
+    public static final String LNAME = "lname";
+    public static final String PHONENO = "phoneno";
+    public static final String SERIALIZEDDATA = "serialized_data";
+    public static final String JSONDATA = "json_data";
+
+}

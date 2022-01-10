7787e36084b6fa6fc2906edc562c0ae1ce49dac4
Rakesh <rakesh@avsstech.com>
2022-01-10 11:54:07 +0530

    Alert Dialog continue...

--- app/src/main/java/com/example/chetufirsttraninig/activities/DialogConceptsActivity.java
+++ app/src/main/java/com/example/chetufirsttraninig/activities/DialogConceptsActivity.java
@@ -3,13 +3,18 @@ package com.example.chetufirsttraninig.activities;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.chetufirsttraninig.R;
 import com.example.chetufirsttraninig.databinding.ActivityDialogConceptsBinding;
+import com.example.chetufirsttraninig.databinding.CustomDialogBinding;
 import com.example.chetufirsttraninig.utility.Utility;
 
 public class DialogConceptsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -27,26 +32,27 @@ public class DialogConceptsActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onClick(View view) {
-        // 1. Alert dialog
+        // TODO : 1. Alert dialog
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle(R.string.app_name);
-        builder.setMessage("This is AlertDialog concept.");
-        builder.setCancelable(false);
-        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Utility.showToast(context, "positive button clicked");
-            }
-        });
+//        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//        builder.setTitle(R.string.app_name);
+//        builder.setMessage("This is AlertDialog concept.");
+//        builder.setCancelable(false);
+//        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Utility.showToast(context, "positive button clicked");
+//            }
+//        });
+//
+//        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Utility.showToast(context, "Negative button clicked");
+//                dialogInterface.dismiss();
+//            }
+//        });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Utility.showToast(context, "Negative button clicked");
-                dialogInterface.dismiss();
-            }
-        });
 //        builder.setNeutralButton("Nothing", new DialogInterface.OnClickListener() {
 //            @Override
 //            public void onClick(DialogInterface dialogInterface, int i) {
@@ -55,10 +61,43 @@ public class DialogConceptsActivity extends AppCompatActivity implements View.On
 //        });
 
 //        builder.show();
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.text_red));
-        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setAllCaps(false);
+
+//        AlertDialog alertDialog = builder.create();
+//        alertDialog.show();
+//        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.text_red));
+//        alertDialog.getButton(DialogInterface.BUTTON_POSITIVE).setAllCaps(false);
+
+        //TODO : 2. Custom Dialog
+
+        CustomDialogBinding bindingDialog = CustomDialogBinding.inflate(getLayoutInflater());
+
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(bindingDialog.getRoot());
+        dialog.setCancelable(false);
+
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        bindingDialog.btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Utility.showToast(context, "Hi "+bindingDialog.etName.getText().toString());
+                dialog.dismiss();
+            }
+        });
+
+        bindingDialog.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(layoutParams);
+
+
 
 
     }
